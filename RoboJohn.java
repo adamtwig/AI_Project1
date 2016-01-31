5412b30159b9f16c298d0ada71a81da34a9dcfb3
John Tunisi <John Tunisi>
2016-01-31 13:49:20 -0500

    Added strafing

--- RoboJohn.java
+++ RoboJohn.java
@@ -1,22 +1,24 @@
 package sample;
 
+import static robocode.util.Utils.normalRelativeAngleDegrees;
+
 import java.awt.Color;
 
+import robocode.*;
 import robocode.AdvancedRobot;
 import robocode.HitByBulletEvent;
 import robocode.HitWallEvent;
 import robocode.ScannedRobotEvent;
 import robocode.WinEvent;
-import static robocode.util.Utils.normalRelativeAngleDegrees;
 
 /**
  * RoboJohn - a robot by John Tunisi
  */
 public class RoboJohn extends AdvancedRobot {
 	
-	private String lastRotation = "";
 	private int moveDirection = 1;
-	private Double lastAbsoluteBearing = -1.0;
+	private int radarDirection = 1;
+	private Double enemyBearing = 0.0;
 	
 	/**
 	 * TrackFire's run method
@@ -29,19 +31,32 @@ public class RoboJohn extends AdvancedRobot {
 		setBulletColor(Color.pink);
 
 		while (true) {
-			turnGunRight(10);
+			radar();
+			strafe();
+			execute();
 		}
 	}
 	
-	private void strafe(Double bearing) {
-		ahead(100);
-		setTurnRight(bearing + 90);
-		
-//		if (getTime() % 20 == 0) {
+	private void radar() {
+		if (enemyBearing == 0.0) {
+			setTurnRadarRight(360);
+		} else {
+			double turn = getHeading() - getRadarHeading() + enemyBearing;
+			turn += 30 * radarDirection;
+			setTurnRadarRight(normalizeBearing(turn));
+			radarDirection *= -1;
+		}
+	}
+	
+	private void strafe() {
+		// always square off against our enemy
+		setTurnRight(normalizeBearing(enemyBearing + 90));
+
+		// strafe by changing direction every 20 ticks
+		if (getTime() % 20 == 0) {
 			moveDirection *= -1;
 			setAhead(150 * moveDirection);
-//		}
-		execute();
+		}
 	}
 
 	/**
@@ -51,23 +66,24 @@ public class RoboJohn extends AdvancedRobot {
 		// Calculate exact location of the robot
 		Double absoluteBearing = getHeading() + e.getBearing();
 		Double bearingFromGun = normalRelativeAngleDegrees(absoluteBearing - getGunHeading());
-
-		strafe(absoluteBearing);
-		lastAbsoluteBearing = absoluteBearing;
+		enemyBearing = e.getBearing();
+		
+		
+		setTurnRight(e.getBearing() + 90);
 		
 		// If it's close enough, fire!
 		if (Math.abs(bearingFromGun) <= 3) {
-			turnGunRight(bearingFromGun);
+			setTurnGunRight(bearingFromGun);
 			// We check gun heat here, because calling fire()
 			// uses a turn, which could cause us to lose track
 			// of the other robot.
 			if (getGunHeat() == 0) {
-				fire(Math.min(3 - Math.abs(bearingFromGun), getEnergy() - .1));
+				setFire(Math.min(3 - Math.abs(bearingFromGun), getEnergy() - .1));
 			}
 		} // otherwise just set the gun to turn.
 		// Note:  This will have no effect until we call scan()
 		else {
-			turnGunRight(bearingFromGun);
+			setTurnGunRight(bearingFromGun);
 		}
 		// Generates another scan event if we see a robot.
 		// We only need to call this if the gun (and therefore radar)
@@ -94,26 +110,37 @@ public class RoboJohn extends AdvancedRobot {
 	 * onHitWall: What to do when you hit a wall
 	 */
 	public void onHitWall(HitWallEvent e) {
-		if (e.getBearing() < -90) {
-			ahead(20);
-		} else if (e.getBearing() >= -90 && e.getBearing() <= 0) {
-			back(20);
-			if (lastRotation.equals("Left")) {
-				turnLeft((Math.random() % 90) + 90);
-			} else {
-				turnRight((Math.random() % 90) + 90);
-			}
-		} else if (e.getBearing() > 0 && e.getBearing() <= 90) { 
-			back(20);
-			if (lastRotation.equals("Left")) {
-				turnLeft((Math.random() % 90) + 90);
-			} else {
-				turnRight((Math.random() % 90) + 90);
-			}
-		} else if (e.getBearing() > 90) {
-			ahead(20);
+		if (getVelocity() == 0) {
+			moveDirection *= -1;
 		}
-	}	
+		
+		strafe();
+//		if (e.getBearing() < -90) {
+//			ahead(20);
+//		} else if (e.getBearing() >= -90 && e.getBearing() <= 0) {
+//			back(20);
+//			if (lastRotation.equals("Left")) {
+//				turnLeft((Math.random() % 90) + 90);
+//			} else {
+//				turnRight((Math.random() % 90) + 90);
+//			}
+//		} else if (e.getBearing() > 0 && e.getBearing() <= 90) { 
+//			back(20);
+//			if (lastRotation.equals("Left")) {
+//				turnLeft((Math.random() % 90) + 90);
+//			} else {
+//				turnRight((Math.random() % 90) + 90);
+//			}
+//		} else if (e.getBearing() > 90) {
+//			ahead(20);
+//		}
+	}
+	
+	double normalizeBearing(double angle) {
+		while (angle > 180) angle -= 360;
+		while (angle < -180) angle += 360;
+		return angle;
+	}
 	
 }
 
