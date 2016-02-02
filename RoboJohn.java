c73319a750af27afaf2293c44905974b380f1713
John Tunisi <John Tunisi>
2016-02-02 14:22:21 -0500

    Moves to center

--- RoboJohn.java
+++ RoboJohn.java
@@ -3,38 +3,49 @@ package sample;
 import static robocode.util.Utils.normalRelativeAngleDegrees;
 
 import java.awt.Color;
+import java.awt.geom.Point2D;
 
-import robocode.*;
 import robocode.AdvancedRobot;
 import robocode.HitByBulletEvent;
 import robocode.HitWallEvent;
 import robocode.ScannedRobotEvent;
 import robocode.WinEvent;
 
-/**
- * RoboJohn - a robot by John Tunisi
- */
 public class RoboJohn extends AdvancedRobot {
 	
 	private int moveDirection = 1;
 	private int radarDirection = 1;
 	private Double enemyBearing = 0.0;
 	
-	/**
-	 * TrackFire's run method
-	 */
 	public void run() {
 		setBodyColor(Color.pink);
 		setGunColor(Color.pink);
 		setRadarColor(Color.pink);
 		setScanColor(Color.pink);
 		setBulletColor(Color.pink);
+		
+		moveToPoint(getBattleFieldCenter());
+		execute();
 
+		boolean first = true;
+		
 		while (true) {
-			radar();
-			strafe();
-			execute();
+			if (!first || getLocation().distance(getBattleFieldCenter()) < 10.0) {
+				radar();
+				strafe();
+				execute();	
+			} else {
+				moveToPoint(getBattleFieldCenter());
+				execute();
+				first = false;
+			}
 		}
+		
+//		while (true) {
+//			radar();
+//			strafe();
+//			execute();
+//		}
 	}
 	
 	private void radar() {
@@ -49,98 +60,92 @@ public class RoboJohn extends AdvancedRobot {
 	}
 	
 	private void strafe() {
-		// always square off against our enemy
 		setTurnRight(normalizeBearing(enemyBearing + 90));
 
-		// strafe by changing direction every 20 ticks
 		if (getTime() % 20 == 0) {
 			moveDirection *= -1;
-			setAhead(150 * moveDirection);
+			setAhead(500 * moveDirection);
 		}
 	}
 
-	/**
-	 * onScannedRobot:  We have a target.  Go get it.
-	 */
 	public void onScannedRobot(ScannedRobotEvent e) {
 		// Calculate exact location of the robot
 		Double absoluteBearing = getHeading() + e.getBearing();
+		Double velocity = e.getVelocity();
 		Double bearingFromGun = normalRelativeAngleDegrees(absoluteBearing - getGunHeading());
 		enemyBearing = e.getBearing();
 		
 		
 		setTurnRight(e.getBearing() + 90);
 		
-		// If it's close enough, fire!
-		if (Math.abs(bearingFromGun) <= 3) {
-			setTurnGunRight(bearingFromGun);
-			// We check gun heat here, because calling fire()
-			// uses a turn, which could cause us to lose track
-			// of the other robot.
-			if (getGunHeat() == 0) {
-				setFire(Math.min(3 - Math.abs(bearingFromGun), getEnergy() - .1));
+		if (velocity < .02 && velocity > -.02) {
+			
+			if (Math.abs(bearingFromGun) <= 3) {
+				setTurnGunRight(bearingFromGun);
+				
+				if (getGunHeat() == 0) {
+					setFire(Math.min(3 - Math.abs(bearingFromGun), getEnergy() - .1));
+				}
+			} else {
+				setTurnGunRight(bearingFromGun);
 			}
-		} // otherwise just set the gun to turn.
-		// Note:  This will have no effect until we call scan()
-		else {
-			setTurnGunRight(bearingFromGun);
-		}
-		// Generates another scan event if we see a robot.
-		// We only need to call this if the gun (and therefore radar)
-		// are not turning.  Otherwise, scan is called automatically.
-		if (bearingFromGun == 0) {
-			scan();
+			if (bearingFromGun == 0) {
+				scan();
+			}
 		}
 	}
 
 	public void onWin(WinEvent e) {
-		// Victory dance
 		turnRight(36000);
 	}
-	
 
-	/**
-	 * onHitByBullet: What to do when you're hit by a bullet
-	 */
 	public void onHitByBullet(HitByBulletEvent e) {
-		
+		changeDirection();
+	}
+
+	public void onHitWall(HitWallEvent e) {
+		changeDirection();
+		strafe();
 	}
 	
-	/**
-	 * onHitWall: What to do when you hit a wall
-	 */
-	public void onHitWall(HitWallEvent e) {
+	private Point2D getLocation() {
+		return new Point2D.Double(getX(), getY());
+	}
+	
+	private Point2D getBattleFieldCenter() {
+		return new Point2D.Double((getBattleFieldWidth() / 2), (getBattleFieldHeight() / 2));
+	}
+	
+	private double absoluteBearingDegrees(Point2D source, Point2D target) {
+        return Math.toDegrees(Math.atan2(target.getX() - source.getX(), target.getY() - source.getY()));
+    }
+    
+    private void moveToPoint(Point2D point) {
+    	Double distanceFromPoint = getLocation().distance(point);
+        Double angle = normalizeBearing(absoluteBearingDegrees(getLocation(), point) - getHeading());
+        
+        if (Math.abs(angle) > 90.0) {
+        	distanceFromPoint *= -1;
+        	
+        	if (angle > 0.0) {
+        		angle -= 180.0;
+        	} else {
+        		angle += 180.0;
+        	}
+        }
+        setTurnRight(angle);
+        setAhead(distanceFromPoint);
+    }
+	
+	private void changeDirection() {
 		if (getVelocity() == 0) {
 			moveDirection *= -1;
 		}
-		
-		strafe();
-//		if (e.getBearing() < -90) {
-//			ahead(20);
-//		} else if (e.getBearing() >= -90 && e.getBearing() <= 0) {
-//			back(20);
-//			if (lastRotation.equals("Left")) {
-//				turnLeft((Math.random() % 90) + 90);
-//			} else {
-//				turnRight((Math.random() % 90) + 90);
-//			}
-//		} else if (e.getBearing() > 0 && e.getBearing() <= 90) { 
-//			back(20);
-//			if (lastRotation.equals("Left")) {
-//				turnLeft((Math.random() % 90) + 90);
-//			} else {
-//				turnRight((Math.random() % 90) + 90);
-//			}
-//		} else if (e.getBearing() > 90) {
-//			ahead(20);
-//		}
 	}
 	
-	double normalizeBearing(double angle) {
+	private double normalizeBearing(double angle) {
 		while (angle > 180) angle -= 360;
 		while (angle < -180) angle += 360;
 		return angle;
 	}
-	
 }
-
